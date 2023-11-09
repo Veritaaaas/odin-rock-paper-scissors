f753a6ad44e266efb0b9b8a0b27117fb62f90adb
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2023-11-09 11:16:48 +0800

    function getComputerChoice is done

---
+++ script.js
@@ -0,0 +1,9 @@
+let choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice()
+{
+    let choice = Math.floor(Math.random() * 3);
+    return choices[choice];
+}
+
+console.log(getComputerChoice());
