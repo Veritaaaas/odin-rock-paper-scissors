de8e6828a36ea583af64330e97e16a7e36a0d435
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2023-11-09 13:35:40 +0800

    function game is done

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let choices = ["rock", "paper", "scissors"];
+let player_wincount = 0;
+let compuer_wincount = 0;
 
 function getComputerChoice()
 {
@@ -17,26 +19,52 @@ function playRound(computer_choice, player_choice)
 
     else if(computer_choice === "rock" && player_choice === "paper")
     {
+        player_wincount++;
         return("Player wins!");
     }
 
     else if(computer_choice === "paper" && player_choice === "scissors")
     {
+        player_wincount++;
         return("Player wins!");
     }
 
     else if(computer_choice === "scissors" && player_choice === "rock")
     {
+        player_wincount++;
         return("Player wins!");
     }
 
     else
     {
+        compuer_wincount++;
         return("Computer wins!");
     }
 }
 
+function game()
+{
+    for (let i = 0; i < 5; i++)
+    {
+        let player_choice = prompt("Rock, Paper, or Scissors?");
+        let computer_choice = getComputerChoice();
+        console.log(playRound(computer_choice, player_choice));
+    }
 
-const playerSelection = "ROCK";
-const computerSelection = getComputerChoice();
-console.log(playRound(computerSelection, playerSelection))
+    if (player_wincount === compuer_wincount)
+    {
+        console.log("IT's a TIE!");
+    }
+
+    else if (player_wincount > compuer_wincount)
+    {
+        console.log("You Win!");
+    }
+
+    else
+    {
+        console.log("You Lose!");
+    }
+}
+
+game();
