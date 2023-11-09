b374d69bd88b396f327cb40153db46c82a436b74
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2023-11-09 11:28:34 +0800

    playRound functions is done

--- script.js
+++ script.js
@@ -6,4 +6,37 @@ function getComputerChoice()
     return choices[choice];
 }
 
-console.log(getComputerChoice());
+function playRound(computer_choice, player_choice)
+{
+    player_choice = player_choice.toLowerCase();
+
+    if (computer_choice === player_choice)
+    {
+        return("It's a tie!");
+    }
+
+    else if(computer_choice === "rock" && player_choice === "paper")
+    {
+        return("Player wins!");
+    }
+
+    else if(computer_choice === "paper" && player_choice === "scissors")
+    {
+        return("Player wins!");
+    }
+
+    else if(computer_choice === "scissors" && player_choice === "rock")
+    {
+        return("Player wins!");
+    }
+
+    else
+    {
+        return("Computer wins!");
+    }
+}
+
+
+const playerSelection = "ROCK";
+const computerSelection = getComputerChoice();
+console.log(playRound(computerSelection, playerSelection))
